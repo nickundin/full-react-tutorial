58875fc990e99c05a799e4bb6bfc6933550df030
nickundin <nickundin@gmail.com>
2023-06-18 20:49:16 -0400

    Added dynamic values to App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  // booleans and objects cannot be outputted with dynamic values
+  // what about arrays doe
+  const link = "'https://www.google.com'";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* emmet: div.content creates div with a className of content */}
+      <div className='content'>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{10}</p>
+        <p>{'hello, ninjas'}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={link}>Google Site</a>
+      </div>
     </div>
   );
 }
 
+// component function gets exported, to be used in other files
 export default App;
