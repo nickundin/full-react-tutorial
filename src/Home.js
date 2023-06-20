7cbfde4092e4a176f03e0852c7b3c9f0fc1062c1
nickundin <nickundin@gmail.com>
2023-06-20 16:19:07 -0400

    Added error handling, updated README

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogList from './BlogList';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   // the useEffect hook runs every time the DOM re-renders
   // this hook has access to the state (in this case, blogs)
@@ -13,15 +14,26 @@ const Home = () => {
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then((res) => {
+        if (!res.ok) {
+          // use res.ok to check if the server response is good
+          // note how throwing the final catch block catches the error thrown here
+          throw Error('could not fetch the data for that resource');
+        }
         return res.json(); // uses the fetch API to parse the json into a JS object
         // note that in db.json, the first "blogs" just indicates the name of the path
         // everything AFTER that is the actual JS object!
       })
       .then((data) => {
+        // again, json() returns a promise, so tack on a .then
         console.log(data);
         setBlogs(data);
         setIsPending(false);
-      }); // again, json() returns a promise, so tack on a .then
+        setError(null); // if you fetch the data successfully, error should then become null
+      })
+      .catch((err) => {
+        setIsPending(false); // in order to avoid showing 'loading' on the error screen
+        setError(err.message);
+      });
   }, []); // empty = only on initial render, not whenever data changes
 
   return (
@@ -30,6 +42,8 @@ const Home = () => {
       {/* thus, you don't want to output Bloglist until there is a value for blogs */}
       {/* by using a logical and, you can check to see if blogs exists first */}
       {/* likewise, you can use it to show a loading message, at least until it is no longer loading */}
+      {/* finally, you can use the error state to show an error message */}
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} />}
     </div>
--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 
 // this file is responsible for mounting all the React components to the DOM
 // takes the App component and renders it to the DOM inside the root element
+// App, the root component, sits as the top of the component tree
+// further components are all nested below App.js (eg. Navar.js)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-    {/* App, the root component, sits as the top of the component tree */}
-    {/* further components are all nested below App.js (eg. Navar.js) */}
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  //  </React.StrictMode>
 );
