1081e1b3b27752859a485bb169b045a84cb9d46b
nickundin <nickundin@gmail.com>
2023-06-20 16:35:57 -0400

    Made custom useFetch hook

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,13 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  // the useEffect hook runs every time the DOM re-renders
-  // this hook has access to the state (in this case, blogs)
-  // DO NOT update state inside useEffect, as that causes an infinite loop
-  // to control when useEffect runs, pass in a "dependency array" containing state values as a second argument
-  // fetch returns a promise, so you can attach a "then" method
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then((res) => {
-        if (!res.ok) {
-          // use res.ok to check if the server response is good
-          // note how throwing the final catch block catches the error thrown here
-          throw Error('could not fetch the data for that resource');
-        }
-        return res.json(); // uses the fetch API to parse the json into a JS object
-        // note that in db.json, the first "blogs" just indicates the name of the path
-        // everything AFTER that is the actual JS object!
-      })
-      .then((data) => {
-        // again, json() returns a promise, so tack on a .then
-        console.log(data);
-        setBlogs(data);
-        setIsPending(false);
-        setError(null); // if you fetch the data successfully, error should then become null
-      })
-      .catch((err) => {
-        setIsPending(false); // in order to avoid showing 'loading' on the error screen
-        setError(err.message);
-      });
-  }, []); // empty = only on initial render, not whenever data changes
+  // with the colon, DATA IS RENAMED TO BLOGS
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className='home'>
---
+++ src/useFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+
+// custom hooks NEED to start with use!
+const useFetch = (url) => {
+  // note that blogs has been renamed data in order to make the custom hook more reusable
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  // the useEffect hook runs every time the DOM re-renders
+  // this hook has access to the state (in this case, blogs)
+  // DO NOT update state inside useEffect, as that causes an infinite loop
+  // to control when useEffect runs, pass in a "dependency array" containing state values as a second argument
+  // fetch returns a promise, so you can attach a "then" method
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          // use res.ok to check if the server response is good
+          // note how throwing the final catch block catches the error thrown here
+          throw Error('could not fetch the data for that resource');
+        }
+        return res.json(); // uses the fetch API to parse the json into a JS object
+        // note that in db.json, the first "blogs" just indicates the name of the path
+        // everything AFTER that is the actual JS object!
+      })
+      .then((data) => {
+        // again, json() returns a promise, so tack on a .then
+        setData(data);
+        setIsPending(false);
+        setError(null); // if you fetch the data successfully, error should then become null
+      })
+      .catch((err) => {
+        setIsPending(false); // in order to avoid showing 'loading' on the error screen
+        setError(err.message);
+      });
+  }, [url]); // url is a dep because whenever the url changes, it will rerun this function to get the data for that endpoint!
+
+  // with custom hooks, you can return any value you want!
+  // an object is better than an array because the order doesn't really matter with destructuring
+  return {
+    data,
+    isPending,
+    error,
+  };
+};
+
+export default useFetch;
