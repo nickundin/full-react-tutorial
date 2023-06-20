8d9260da12a6b668c2032fff51bc353a307fc800
nickundin <nickundin@gmail.com>
2023-06-19 20:09:43 -0400

    Added name and age state

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,25 @@
+import { useState } from 'react';
+
 const Home = () => {
-  const handleClick = (e) => {
-    console.log('hello, ninjas', e);
+  // the two parts of useState
+  // the value itself, and a function that's used to change that value
+  // changing the state forces React to re-render that particular component
+  // note that the state value can be a string, object, array, etc.
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    setName('luigi');
+    setAge(30);
   };
 
-  const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target);
-  };
-
-  // NOTE THE USE OF AN ANONYMOUS FUNCTION BELOW IN ORDER TO REFERENCE THE HANDLECLICKAGAIN FUNCTION
-  // because handleClickAgain is wrapped in an anonymous function, which isn't firing to begin with
-  // when the user clicks the buttoan, it fires the function and then invokes handleClickAgain
-  // finally, note how the event object is passed into the custom handleClickAgain function as a second argument
   return (
     <div className='home'>
       <h2>Homepage</h2>
+      <p>
+        {name} is {age} years old
+      </p>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain('mario', e)}>
-        Click me again
-      </button>
     </div>
   );
 };
