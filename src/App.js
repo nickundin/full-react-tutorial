cfd586c7247a8d6951b637f399c3e1ad63d3974c
nickundin <nickundin@gmail.com>
2023-06-21 17:27:28 -0400

    Added route parameters for individual blogs

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 // to grant access to the router for all components nested inside the App (root) component, surround App with Router
 // the Switch component ensures only one route shows at any one time (note how Navbar always shows, despite the route)
+// note the use of :id as a route parameter!
 function App() {
   return (
     <Router>
@@ -17,6 +19,9 @@ function App() {
             </Route>
             <Route path='/create'>
               <Create />
+            </Route>
+            <Route path='/blogs/:id'>
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 // note that the blogs data is defined in Home.js, so it must be passed in somehow
 // this can be done either by copying and pasting the data into BlogList, or passing it in via props
 // using props is BETTER because the blogs data makes more sense in the Home component
@@ -10,9 +11,12 @@ const BlogList = ({ blogs }) => {
       {blogs.map((blog) => (
         // ALWAYS add a unique key attribute to each item that you output!
         // note the use of the MAP method, rather than a for-of loop, to output the blogs stored in state
+        // also note the use of template literal + ${} to provide the route parameter to the Link
         <div className='blog-preview' key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
