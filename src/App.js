9edd2b23a53331f2fdf175f41d7688adcb9e5d4e
nickundin <nickundin@gmail.com>
2023-06-20 21:18:21 -0400

    Installed react router and added Switch component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+// to grant access to the router for all components nested inside the App (root) component, surround App with Router
+// the Switch component ensures only one route shows at any one time (note how Navbar always shows, despite the route)
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-      <div className='content'>
-        <Home />
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
